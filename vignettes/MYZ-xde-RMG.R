ebfb37333b47cb521b0d66b4874c588c49792c89
David Smith <smitdave@gmail.com>
2024-06-10 10:56:50 -0700

    fix this

---
+++ vignettes/MYZ-xde-RMG.R
@@ -0,0 +1,16 @@
+## -----------------------------------------------------------------------------
+EIPo = list(EIPname = "static_xde", eip=12) 
+model <- xde_setup(MYZname="RMG_xde", Lname="trace", Xname = "trace", EIPopts = EIPo, nPatches=3)
+
+## -----------------------------------------------------------------------------
+model <- xde_solve(model)
+
+## ----fig.height=7.5, fig.width=5.5--------------------------------------------
+par(mfrow = c(2,1))
+xds_plot_M(model)
+xds_plot_YZ(model, add_axes = F)
+xds_plot_YZ_fracs(model)
+
+## -----------------------------------------------------------------------------
+xds_plot_YZ_fracs(model)
+
