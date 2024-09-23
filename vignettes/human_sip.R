7d166d64050c707b6477f773165b45ff1e20998f
David Smith <smitdave@gmail.com>
2024-09-23 14:16:15 -0700

    new models added
    five models were moved from ramp.xds
    + adult-SEI
    + human-SEIS
    + human-SEISd
    + human-SIP
    + human-SIPd

---
+++ vignettes/human_sip.R
@@ -0,0 +1,93 @@
+## ----include = FALSE----------------------------------------------------------
+knitr::opts_chunk$set(
+  collapse = TRUE,
+  comment = "#>"
+)
+
+## ----message=FALSE, warning=FALSE---------------------------------------------
+library(ramp.xds)
+library(ramp.library)
+library(deSolve)
+library(data.table)
+library(ggplot2)
+
+## ----echo=FALSE---------------------------------------------------------------
+#devtools::load_all()
+
+## -----------------------------------------------------------------------------
+nStrata <- 3
+H <- c(100, 500, 250)
+nPatches <- 3
+residence <- 1:3 
+params <- make_xds_template("ode", "human", nPatches, 1, residence) 
+
+## -----------------------------------------------------------------------------
+b <- 0.55
+c <- 0.15
+r <- 1/200
+eta <- c(1/30, 1/40, 1/35)
+rho <- c(0.05, 0.1, 0.15)
+xi <- rep(0, 3)
+Xo = list(b=b,c=c,r=r,eta=eta,rho=rho,xi=xi)
+class(Xo) <- "SIP"
+
+## -----------------------------------------------------------------------------
+eir <- 2/365
+xde_steady_state_X(eir*b, H, Xo) ->ss
+ss
+
+## -----------------------------------------------------------------------------
+Xo$I <- ss$I
+Xo$P <- ss$P
+
+## -----------------------------------------------------------------------------
+params = make_Xpar("SIP", params, 1, Xo) 
+params = make_Xinits(params, H, 1, Xo)
+
+## -----------------------------------------------------------------------------
+MYZo = list(
+  MYZm = eir*H
+)
+
+## -----------------------------------------------------------------------------
+params = make_MYZpar("trivial", params, 1, MYZo)
+params = make_MYZinits(params, 1)
+params <- setup_Hpar_static(params, 1)
+params = make_Lpar("trivial", params, 1)
+params = make_Linits(params, 1)
+
+## -----------------------------------------------------------------------------
+params = make_indices(params)
+
+## -----------------------------------------------------------------------------
+xde_steady_state_X(eir*b, H, params$Xpar[[1]])
+
+## -----------------------------------------------------------------------------
+y0 <- as.vector(unlist(get_inits(params)))
+
+## -----------------------------------------------------------------------------
+out <- deSolve::ode(y = y0, times = c(0, 365), xde_derivatives, parms= params, method = 'lsoda') 
+out1<- out
+
+## ----out.width = "100%"-------------------------------------------------------
+colnames(out)[params$ix$X[[1]]$S_ix+1] <- paste0('S_', 1:params$nStrata)
+colnames(out)[params$ix$X[[1]]$I_ix+1] <- paste0('I_', 1:params$nStrata)
+colnames(out)[params$ix$X[[1]]$P_ix+1] <- paste0('P_', 1:params$nStrata)
+
+out <- as.data.table(out)
+out <- melt(out, id.vars = 'time')
+out[, c("Component", "Strata") := tstrsplit(variable, '_', fixed = TRUE)]
+out[, variable := NULL]
+
+ggplot(data = out, mapping = aes(x = time, y = value, color = Strata)) +
+  geom_line() +
+  facet_wrap(. ~ Component, scales = 'free') +
+  theme_bw()
+
+## -----------------------------------------------------------------------------
+xds_setup_human(Xname="SIP", nPatches=3, residence = 1:3, HPop=H, Xopts = Xo, MYZopts = MYZo) -> test_SIP_xde
+
+## -----------------------------------------------------------------------------
+xds_solve(test_SIP_xde, 365, 365)$outputs$orbits$deout -> out2
+approx_equal(out2,out1) 
+
