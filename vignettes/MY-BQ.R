de8c44abbb69bb7503312c5bbff4f0ce3a0f703f
David L Smith <smitdave@gmail.com>
2026-08-21 17:59:33 -0700

    devtools removed

--- vignettes/MY-BQ.R
+++ vignettes/MY-BQ.R
@@ -5,9 +5,6 @@ library(deSolve)
 library(ramp.library)
 
 ## -----------------------------------------------------------------------------
-devtools::load_all()
-
-## -----------------------------------------------------------------------------
 HPop = rep(1000, 3)
 residence = c(1:3) 
 model <- xds_setup(MYname="BQ", Lname="trivial", Xname = "trivial", residence=residence, HPop=HPop, nPatches=3)
